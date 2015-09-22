5667f9eff3bdd5fed2e64bde00263a83679fe451
erikwang <erikwang@ADMINIB-8I58R22.mkm.can.ibm.com>
2015-09-22 18:04:43 -0400

    1. Migrate python script to Java, now VemkdPerfWidget can load a vemkd log to database. 2. The logic is the same to previous Python one 3. Add more information/interaction on SI shell 4. Enhanced constructor of VemkdPerfWidget 5. Widget and SI shell now can read xml configuration files

--- src/com/ibm/stg/pc/sym/si/util/VemkdPerfWidget.java
+++ src/com/ibm/stg/pc/sym/si/util/VemkdPerfWidget.java
@@ -1,14 +1,32 @@
 package com.ibm.stg.pc.sym.si.util;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.ibm.stg.pc.sym.si.widget.Widget;
 import com.mysql.jdbc.Connection;
 
 public class VemkdPerfWidget implements Widget {
 	Connection conn = null;
+	String logurl;
+	String pmr;
+	String dbtable;
+	boolean wrflag;
+	
+	VemkdPerfWidget(){
+		verifyWidget();
+		XmlReader xmlReader= new XmlReader();
+		dbtable = xmlReader.getFromConf("DBTABLE","src\\conf.xml");
+	}
+	
 	@Override
 	public void runWidget() {
 		doSomeTest();
@@ -17,7 +35,8 @@ public class VemkdPerfWidget implements Widget {
 	@Override
 	public void verifyWidget() {
 		// TODO Auto-generated method stub
-
+		System.out.println("Verifying this widget...");
+		System.out.println("Load From Database: This script will log vemkd performance logs into database");
 	}
 
 	@Override
@@ -74,5 +93,78 @@ public class VemkdPerfWidget implements Widget {
 		
 	}
 	
+	public void loadFromLog(){
+		String str = null;
+        FileReader reader = null;
+        String[] perfitem;
+        String[] perfvalues =new String[16];
+		try {
+			reader = new FileReader(logurl);
+			BufferedReader br = new BufferedReader(reader);
+			while((str = br.readLine()) != null) {
+				Pattern pattern = Pattern.compile("\\<(.*?)\\>");
+				perfitem = str.split(" ");
+				if(perfitem.length >= 5	&& perfitem[5].equals("printPerf:")){
+					System.out.print(".");
+					Matcher matcher = pattern.matcher(str);
+					int i = 0;
+					while (matcher.find()){
+						perfvalues[i] = matcher.group(1);
+						i++;
+						//System.out.println(matcher.group(1));
+					}
+					String sql = "INSERT INTO "+dbtable+
+                    "(logdate,opcode,totalcnt,totaltime,maxtime,mintime,totalfileio,maxfileio,minfileio,"+
+                    "totaliocounter,totalchanopentime,maxchanopentime,minchanopentime,func,PMR)"+
+                    " values( SYSDATE(),"+perfvalues[0]+","+perfvalues[1]+","+perfvalues[2]+","+perfvalues[3]+","+perfvalues[4]+","+perfvalues[5]+","+perfvalues[6]+","+perfvalues[7]+","+perfvalues[8]+","+perfvalues[9]+","+perfvalues[10]+","+perfvalues[11]+",'"+perfvalues[12]+"','"+pmr+"')";
+					//System.out.println(sql);
+					if (wrflag){
+						try{
+							Statement stmt2 = conn.createStatement();
+							stmt2.executeUpdate(sql);
+							stmt2.close();
+						}catch(SQLException e){
+							e.printStackTrace();
+						}	
+					}
+				}
+			}
+			br.close();
+			reader.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("There is an error while loading vemkd performance file, either file is not exist or can't be operated");
+			e.printStackTrace();
+		}
+		System.out.println();
+		System.out.println("Job finished");
+	}
 
+	public void getInfoFromConsole(){
+		Scanner sca = new Scanner(System.in);
+		System.out.println("1. Please input log directory, default = c:\\vemkd.log");
+		logurl = sca.nextLine();
+		if(logurl.equals("")){
+			logurl = "c:\\vemkd.log";
+		}
+		System.out.println("Log url set to ["+logurl+"]");
+		
+		System.out.println("2. Please input pmr number, default = PMR9999 for general test");
+		pmr = sca.nextLine();
+		if(pmr.equals("")){
+			pmr = "PMR9999";
+		}
+		System.out.println("PMR set to ["+pmr+"]");
+		
+		System.out.println("3. Do you want to write to database, default = N");
+		String strwrflag = sca.nextLine();
+		if(strwrflag.equals("y")){
+			wrflag = true;
+		}else{
+			wrflag = false;
+		}
+		System.out.println("Write flag set to ["+wrflag+"]");
+		
+		loadFromLog();
+	}
 }
--- src/com/ibm/stg/pc/sym/si/util/sishell.java
+++ src/com/ibm/stg/pc/sym/si/util/sishell.java
@@ -3,17 +3,24 @@ package com.ibm.stg.pc.sym.si.util;
 import java.util.Scanner;
 
 import com.mysql.jdbc.Connection;
+import com.ibm.stg.pc.sym.si.widget.*;
 
 public class sishell {
 	static Dbconn db;
 	public static void main(String[] args) {
 		Scanner sca = new Scanner(System.in);
 		Connection conn = null;
+		
+		String dbtable;
+		loadConfig("/conf.xml");
+		
+		
 		// TODO Auto-generated method stub
 		System.out.println("Hello SI!");
 		try {
 			db = new Dbconn("/proxool.xml");
 			conn = db.getConn();
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -21,10 +28,9 @@ public class sishell {
 		while(true){
 			System.out.println("> Please enter your command / ? for help");
 			String name = sca.nextLine();
-			
 			switch(name){
 				case "?":
-					//ShowHelp();
+					ShowHelp();
 					break;
 				case "cdb":
 					break;
@@ -33,8 +39,21 @@ public class sishell {
 					vpwidget.getDbConnection(conn);
 					vpwidget.runWidget();
 					break;
+				case "ll":
+					System.out.println("Entering Load Log From Vemkd log file");
+					VemkdPerfWidget vpwidget2 = new VemkdPerfWidget();
+					vpwidget2.getDbConnection(conn);
+					vpwidget2.getInfoFromConsole();
+					//loadFromLog("c:\\test1.log","PMR8888","sym.vemkd",false);
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+					break;
 				case "by":
-					System.out.println("bye - thank you for use");
+					System.out.println("bye - thank you for using this tool");
 					return;
 					
 				default:
@@ -43,11 +62,13 @@ public class sishell {
 		}
 	}
 	
-	public void loadConfig(String _confFile){
-		XmlReader xmlReader= new XmlReader();
-		String suser = xmlReader.getFromConf("SUSER",_confFile);
-		String duser = xmlReader.getFromConf("DUSER",_confFile);
-		String spswd = xmlReader.getFromConf("SPSWD",_confFile);
-		String dpswd = xmlReader.getFromConf("DPSWD",_confFile);
+	public static void loadConfig(String _confFile){
+		System.out.println("Now loading any configuration from conf.xml...");
+		//XmlReader xmlReader= new XmlReader();
+		//String dbtable = xmlReader.getFromConf("DBTABLE",_confFile);
+	}
+	
+	public static void ShowHelp(){
+		System.out.println("Available command: ll - Load log | l - general database access test | by - quit");
 	}
 }
